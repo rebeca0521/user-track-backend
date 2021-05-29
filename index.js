50683119e927aa1f01dc6f44ec8ffcfe08738d4f
rebeca0521 <rebeca0521@gmail.com>
2021-05-29 17:26:33 +0800

    heroku test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 8000
+const PORT = 5000
 
 express()
   .get('/', (req, res) => res.send('Hello World!'))
