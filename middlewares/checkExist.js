f08c50afbec22227515b33eda62565084c624696
rebeca0521 <rebeca0521@gmail.com>
2021-05-30 20:54:31 +0800

    user-track-backend api

--- middlewares/checkExist.js
+++ middlewares/checkExist.js
@@ -2,6 +2,9 @@ var User = require('../models/userModel')
 
 exports.checkId = async (req, res, next) => {
     const id = req.query.id
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+        return res.status(422).json({message: "Id is not exist."})
+    }
     checkId = await User.exists({_id: id})
     if(!checkId) {
         return res.status(422).json({message: "Id is not exist."})
